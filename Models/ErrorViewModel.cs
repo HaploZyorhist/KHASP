1d4a43c15c4a04aaf078b0c337926092cddba0ea
Zyorhist <49216370+HaploZyorhist@users.noreply.github.com>
2020-10-19 07:54:40 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KHASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
